2cba514a4427bfeb8a9a8215173e0142765fe6c5
Blogg9ggg <2361915181@qq.com>
2020-02-29 17:18:30 +0800

    a wordgame

---
+++ wordgame_1.py
@@ -0,0 +1,8 @@
+print('--------------------Blogggggg----------------------------')
+temp = input("猜猜我现在心里想的是哪个数字：")
+guess = int(temp)
+if guess == 8:
+    print("答案正确！")
+else:
+    print("答案错误！")
+print("游戏结束。")
